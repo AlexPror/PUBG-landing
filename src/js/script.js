94958c7e4a0c6612bfe2f5dc2547cc7f3fa2843f
Alexandr Vorobjev <vorobjev.alexandr-2017@yandex.ru>
2023-10-16 16:31:46 +0300

    add accordion

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,6 @@
-const sliderLine = document.querySelectorAll(".slider__item");
-const prevButton = document.querySelector(".slider__btn-prev");
-const nextButton = document.querySelector(".slider__btn-next");
-
-let sliderIndex = 0;
-const sliderCount = sliderLine.length;
-
-function showNextSlide() {
-  sliderIndex = (sliderIndex + 1) % sliderCount;
-  updateSlider();
-}
-
-function showPrevSlide() {
-  sliderIndex = (sliderIndex - 1 + sliderCount) % sliderCount;
-  updateSlider();
-}
-
-prevButton.addEventListener("click", () => {
-  showPrevSlide();
-});
-
-nextButton.addEventListener("click", () => {
-  showNextSlide();
-});
-
-function updateSlider() {
-  sliderLine.forEach((slide, index) => {
-    if (index === sliderIndex) {
-      slide.classList.add("slider__item--active");
-    } else {
-      slide.classList.remove("slider__item--active");
-    }
+const accordionArray = document.getElementsByClassName("accordion-wrapper");
+for (i = 0; i < accordionArray.length; i++) {
+  accordionArray[i].addEventListener("click", function () {
+    this.classList.toggle("active");
   });
 }
-
-// updateSlider();
