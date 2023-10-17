e5a47952d7ae907702219e99d223b214b789bc47
Egor <koropeczegor@gmail.com>
2023-10-17 11:56:30 +0300

    fix accordion

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 const accordionArray = document.getElementsByClassName("accordion-wrapper");
+
 for (i = 0; i < accordionArray.length; i++) {
   accordionArray[i].addEventListener("click", function () {
     this.classList.toggle("active");
